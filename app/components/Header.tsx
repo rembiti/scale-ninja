857da2e4429aaf7146071d2ee265a1b4c8079533
engageintellect <jc9361@gmail.com>
2025-08-12 17:56:56 -0700

    cleaning up

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Header() {
   return (
     <div className="mb-4">
-      <h1 className="text-5xl md:text-7xl md:text-6xl font-bold text-neutral-100 mb-2">
+      <h1 className="text-5xl md:text-6xl text-neutral-100 mb-2">
         scale-ninja
       </h1>
     </div>
