97d4fb720a489a9beeed70c305a7a216e5987171
engageintellect <jc9361@gmail.com>
2025-08-12 19:04:45 -0700

    adding icons to ui

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 export function Header() {
   return (
     <div className="mb-4">
-      <h1 className="text-5xl md:text-6xl text-neutral-100 mb-2">
+      <h1 className="text-5xl md:text-6xl text-neutral-100 mb-2 flex items-center gap-4">
+        <Icon
+          icon="game-icons:guitar-head"
+          className="w-12 h-12 md:w-16 md:h-16 text-emerald-500"
+        />
         scale-ninja
       </h1>
     </div>
